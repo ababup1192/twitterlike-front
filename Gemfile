61aecf3af249148f3ee38a9c17d2206b3afb1bc4
ababup1192 <ababup1192@gmail.com>
2016-09-30 11:00:16 +0900

    Implement Login Form

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ source "https://rubygems.org"
 gem 'test-unit'
 gem 'sinatra'
 gem 'sinatra-contrib'
+
+group :development do
+  gem 'sinatra-contrib', require: 'sinatra/reloader'
+end
--- src/app.rb
+++ src/app.rb
@@ -7,8 +7,27 @@ class MainApp < Sinatra::Base
     register Sinatra::Reloader
   end
   get '/' do
-    @title = 'hello'
-    @message = 'Hello, World!'
+    # redirect '/login'
+
+    @title = 'Twitterlike'
+    @message = 'Hello, Twitterlike'
+    @styles = []
     erb :index
+  end
+
+  get '/login' do
+    @title = 'Twitterlike -Login-'
+    @styles = ['login.css']
+    erb :login
+  end
+
+  post '/login' do
+    name = params[:name]
+    pass = params[:password]
+    if name == 'abc' && pass == 'password'
+      redirect '/'
+    else
+      redirect '/login'
+    end
   end
 end
