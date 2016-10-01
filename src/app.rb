3b648356c13e974f3ff0072ff2a894a854804571
ababup1192 <ababup1192@gmail.com>
2016-10-01 18:05:23 +0900

    Implement login function

--- src/app.rb
+++ src/app.rb
@@ -1,18 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+require 'net/http'
+require 'uri'
 
 # Sinatra Main controller
 class MainApp < Sinatra::Base
+  use Rack::Session::Pool, expire_after: 2_592_000
   configure :development do
     register Sinatra::Reloader
   end
-  get '/' do
-    # redirect '/login'
 
-    @title = 'Twitterlike'
-    @message = 'Hello, Twitterlike'
-    @styles = []
-    erb :index
+  get '/' do
+    id = session[:id]
+    token = session[:token]
+
+    response = post_request('users/auth/token', id: id, token: token)
+    body = JSON.parse(response.body, symbolize_names: true)
+    if response.code.to_i == 200
+      @title = 'Twitterlike'
+      @message = body.to_s
+      @styles = []
+      erb :index
+    else
+      redirect '/login'
+    end
   end
 
   get '/login' do
@@ -24,10 +36,56 @@ class MainApp < Sinatra::Base
   post '/login' do
     name = params[:name]
     pass = params[:password]
-    if name == 'abc' && pass == 'password'
-      redirect '/'
+    if name.empty? == false || pass.empty? == false
+      response = post_request('users/auth', name: name, password: pass)
+      body = JSON.parse(response.body, symbolize_names: true)
+      if response.code.to_i == 200
+        session[:id] = body[:id]
+        session[:token] = body[:token]
+        redirect '/'
+      else
+        # id & pass 間違い
+        redirect '/login'
+      end
     else
+      # 空
       redirect '/login'
     end
+  end
+
+  get '/signup' do
+    @title = 'Twitterlike -Sign up-'
+    @styles = ['signup.css']
+    erb :signup
+  end
+
+  post '/signup' do
+    session[:signup_err] = nil
+    name = params[:name]
+    pass = params[:password]
+    if name.empty? == false || pass.empty? == false
+      response = post_request('users', name: name, password: pass)
+      body = JSON.parse(response.body, symbolize_names: true)
+      if response.code.to_i == 200
+        session[:id] = body[:id]
+        session[:token] = body[:token]
+        redirect '/'
+      else
+        redirect 'signup'
+      end
+    else
+      session[:signup_err] = :empty_form
+      session[:error_mes] = 'Name or Password is empty.'
+      redirect 'signup'
+    end
+  end
+
+  private def post_request(path, body)
+    uri_str = "http://localhost:9393/#{path}"
+    uri = URI.parse(uri_str)
+    req = Net::HTTP::Post.new(uri.path,
+                              'Content-Type' => 'application/json')
+    req.body = body.to_json
+    Net::HTTP.start(uri.host, uri.port) { |http| http.request(req) }
   end
 end
